5d4fe39a4c6655f41d1e9ffb9a8603f8a462b3ca
kuryu920623 <dj.kuryu0623@gmail.com>
2019-01-23 15:40:18 +0900

    OneMatchの先頭にコメントを追加

--- CulcBoardStateNumbers.py
+++ CulcBoardStateNumbers.py
@@ -1,3 +1,5 @@
+#DeepLearningのブランチを作成
+
 from Reserved6561Tuples.TurnableRange_ForBlack import TurnableRange_ForBlack
 from Reserved6561Tuples.TurnableRange_ForWhite import TurnableRange_ForWhite
 from Reserved6561Tuples.PutablePositions_ForBlack import PutablePositions_ForBlack
